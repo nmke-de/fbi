e551d09802e7659deb0b3e9a5902fdf5af164d21
nmke-de <github@nmke.de>
2023-06-20 01:20:35 +0200

    Added more structure to main()

--- main.c
+++ main.c
@@ -1,19 +1,36 @@
 #include <unistd.h>
 #include <string.h>
+#include "print/print.h"
 
 int main(int argc, char **argv) {
 	char *packagename;
+	int ok = 0;
+	int (*fetch) (char *);
+	int (*build) (char *);
+	int (*install) (char *);
 	// Argparse
 	/**/
 	
 	// Fetch
-	fetch(packagename);
+	ok = fetch(packagename);
+	if (!ok) {
+		logln("Error when fetching ", packagename, ".");
+		return 1;
+	}
 	
 	// Build
-	build(packagename);
+	ok = build(packagename);
+	if (!ok) {
+		logln("Error when building ", packagename, ".");
+		return 2;
+	}
 	
 	// Install
-	install(packagename);
+	ok = install(packagename);
+	if (!ok) {
+		logln("Error when installing ", packagename, ".");
+		return 3;
+	}
 	
 	return 0;
 }
