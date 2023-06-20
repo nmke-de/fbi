68e42b1e52bd369ac251fa97779f88e5b6592019
nmke-de <github@nmke.de>
2023-06-20 02:51:43 +0200

    Added curl.c

---
+++ curl.c
@@ -0,0 +1,18 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include "print/print.h"
+
+#define sysp(args) execvp(*(args), (args))
+
+int curl(const char *url) {
+	pid_t pid = fork();
+	if (pid < 0)
+		return 0;
+	else if (pid == 0) {
+		sysp(cargs("curl", "-LO", url));
+		_exit(1);
+	}
+	if (waitpid(pid, 0, 0) == -1)
+		return 0;
+	return 1;
+}
