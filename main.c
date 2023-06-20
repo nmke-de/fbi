93c5db557309da8712eeb6025fbda732a6a4f7c1
nmke-de <github@nmke.de>
2023-06-20 18:20:49 +0200

    Updated main()

--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <string.h>
 #include "print/print.h"
 
+#include "fbi.h"
+
 int main(int argc, char **argv) {
+	char *url;
 	char *packagename;
 	int ok = 0;
-	int (*fetch) (char *);
-	int (*build) (char *);
-	int (*install) (char *);
+	int (*fetch) (char *) = default_fetch;
+	int (*build) (char *) = default_build;
+	int (*install) (char *) = default_install;
+	
 	// Argparse
-	/**/
+	url = argv[1]; //temporary
+	packagename = basename(url);
 	
 	// Fetch
-	ok = fetch(packagename);
+	ok = fetch(url);
 	if (!ok) {
 		logln("Error when fetching ", packagename, ".");
 		return 1;
 	}
-	
+
 	// Build
 	ok = build(packagename);
 	if (!ok) {
